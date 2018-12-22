3c883a2f3925733309e346680c837734aa5a063c
obsequent <42618406+obsequent@users.noreply.github.com>
2018-12-22 00:09:31 -0500

    Add project files.

---
+++ motadblock/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace motadblock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enableButton = new System.Windows.Forms.Button();
+            this.disableButton = new System.Windows.Forms.Button();
+            this.statusTextBefore = new System.Windows.Forms.Label();
+            this.statusText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enableButton
+            // 
+            this.enableButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.enableButton.Location = new System.Drawing.Point(12, 39);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(468, 200);
+            this.enableButton.TabIndex = 0;
+            this.enableButton.Text = "Enable";
+            this.enableButton.UseVisualStyleBackColor = true;
+            this.enableButton.Click += new System.EventHandler(this.enableButton_Click);
+            // 
+            // disableButton
+            // 
+            this.disableButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.disableButton.Location = new System.Drawing.Point(12, 245);
+            this.disableButton.Name = "disableButton";
+            this.disableButton.Size = new System.Drawing.Size(468, 200);
+            this.disableButton.TabIndex = 1;
+            this.disableButton.Text = "Disable";
+            this.disableButton.UseVisualStyleBackColor = true;
+            this.disableButton.Click += new System.EventHandler(this.disableButton_Click);
+            // 
+            // statusTextBefore
+            // 
+            this.statusTextBefore.AutoSize = true;
+            this.statusTextBefore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.statusTextBefore.Location = new System.Drawing.Point(12, 13);
+            this.statusTextBefore.Name = "statusTextBefore";
+            this.statusTextBefore.Size = new System.Drawing.Size(154, 26);
+            this.statusTextBefore.TabIndex = 2;
+            this.statusTextBefore.Text = "Current status:";
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.statusText.ForeColor = System.Drawing.Color.Red;
+            this.statusText.Location = new System.Drawing.Point(172, 13);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(305, 26);
+            this.statusText.TabIndex = 3;
+            this.statusText.Text = "FUCKED - CAN\'T READ FILE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(492, 457);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.statusTextBefore);
+            this.Controls.Add(this.disableButton);
+            this.Controls.Add(this.enableButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "motdadblock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.Button disableButton;
+        private System.Windows.Forms.Label statusTextBefore;
+        private System.Windows.Forms.Label statusText;
+    }
+}
+
---
+++ motadblock/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace motadblock
+{
+    public partial class Form1 : Form
+    {
+        public string hostsPath = Environment.GetEnvironmentVariable("SYSTEMROOT")+"\\system32\\drivers\\etc\\hosts";
+        public string currentHosts = motadblock.Properties.Resources.hostslist; // yes, I know this should be in another file, but this is way too convenient 
+        // error codes
+        // 1 = can't open hosts
+        // 2 = can't write to hosts
+        // 3 = can't delete from hosts
+        // 4 = error telling status
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void showEnabled() {
+            statusText.ForeColor = System.Drawing.Color.Lime;
+            statusText.Text = "ENABLED";
+        }
+        public void showDisabled()
+        {
+            statusText.ForeColor = System.Drawing.Color.Orange;
+            statusText.Text = "DISABLED";
+        }
+        public void disableButtons()
+        {
+            enableButton.Enabled = false;
+            disableButton.Enabled = false;
+        }
+        public void showFucked(String error)
+        {
+            statusText.ForeColor = System.Drawing.Color.Red;
+            statusText.Text = "FUCKED - " + error;
+            // disableButtons(); // (temporary for debugging)
+        }
+        public void enableButton_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = File.Open(hostsPath, FileMode.Append, FileAccess.Write, FileShare.None)) {
+                fs.Write(new UTF8Encoding(true).GetBytes(currentHosts), 0, new UTF8Encoding(true).GetBytes(currentHosts).Length);
+        }
+            setStatus();
+        }
+
+        public void disableButton_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(hostsPath, Regex.Replace(File.ReadAllText(hostsPath), @"(?s)\r\n#motadblock - do not add anything until after #end of motadblock.*#end of motadblock - add things under here", @""));
+            setStatus();
+        }
+        public void setStatus()
+        {
+            bool enabled = false;
+            string[] lines = System.IO.File.ReadAllLines(hostsPath);
+            foreach (string line in lines)
+            {
+                if (line.Contains("motadblock"))
+                {
+                    enabled = true;
+                    break;
+                }
+            }
+            if (enabled)
+            {
+                showEnabled();
+            }
+            else
+            {
+                showDisabled();
+            }
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            enableButton.Font = new Font(enableButton.Font.FontFamily, 72);
+            disableButton.Font = new Font(disableButton.Font.FontFamily, 72);
+            setStatus();
+        }
+    }
+}
